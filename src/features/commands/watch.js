3cf3cf056555dd70aa6c2825fad384e38491443e
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-06 19:20:00 +0200

    Add /watch command

---
+++ src/features/commands/watch.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+      db = require('../../db');
+
+module.exports = {
+    type: 'slash',
+    data: new SlashCommandBuilder()
+        .setName('watch')
+        .setDescription('Sends a direct message whenever keyword that you track gets mentioned.')
+        .addStringOption(option =>
+            option.setName('keyword')
+                .setDescription('The keyword you track.')
+                .setRequired(true),
+    async execute (client, interaction) {
+        const keyword = interaction.options.get("keyword");
+        const server = interaction.guild;
+        const refreshWatchedCollection = () => (
+             watchedKeywordsCollection = db.getWatchedKeywords()
+        );
+        if (keyword.length < 3) {
+          interaction.reply({ content: `Keyword ${keyword} cannot be watched because the keyword has fewer than 3 characters.`, ephemeral: true });
+        } else {
+          try {
+            db.watchKeyword(message.author.id, server.id, trackingWord).then(resp => {
+              refreshWatchedCollection().then(resp => db.getWatchedKeywords(message.author.id, server.id).then(keywords => {
+                const list = keywords[0].watchedWords.length === 6 ? keywords[0].watchedWords.slice(1) : keywords[0].watchedWords
+                const userToDM = client.users.cache.get(interaction.member.user.id);
+                userToDM.send(`\`${keyword}\` keyword tracking is set up successfully on **${server.name}** server.\nCurrently tracked server keywords:\n${list.map((keyword, index) => `${index + 1}. ${keyword} \n`).join('')}\nYou can track up to 5 keywords.`).catch({});
+              }))
+            });
+          } catch (error) {
+            interaction.reply({ content: `You must allow direct messages from members in this server for this feature to work.\nEnable direct messages in **Privacy Settings > Allow direct messages from other members**`, ephemeral: true });
+          }
+        }
+    },
+};
