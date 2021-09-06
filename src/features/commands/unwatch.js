e078a6b54568a7ac083fdcc8fac738e91d04e1fc
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-06 22:05:18 +0200

    Update unwatch.js

--- src/features/commands/unwatch.js
+++ src/features/commands/unwatch.js
@@ -19,7 +19,7 @@ module.exports = {
         );
         if (keyword) {
            try {
-             db.removeWatchedKeyword(interaction.member.id, server.id, keyword).then(resp => {
+             db.removeWatchedKeyword(interaction.member.id, server.id, [keyword]).then(resp => {
                  refreshWatchedCollection();
                  interaction.reply({ content: `Keyword removed successfully.`, ephemeral: true });
              });
