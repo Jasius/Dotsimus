af9d45383105d73627e9cedc9e3aaac1fabfd417
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-06 20:51:05 +0200

    Add /unwatch command

---
+++ src/features/commands/unwatch.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+      db = require('../../db');
+
+module.exports = {
+    type: 'slash',
+    data: new SlashCommandBuilder()
+        .setName('unwatch')
+        .setDescription('Removes a keyword or all keywords that you have watched before.')
+        .addStringOption(option =>
+            option.setName('keyword')
+                .setDescription('The keyword you will untrack, if keyword is unspecified then Dotsimus will remove all of your tracked keywords.')
+        ),
+    async execute (client, interaction) {
+        const keyword = interaction.options.getString("keyword");
+        const server = interaction.guild;
+        let watchedKeywordsCollection = db.getWatchedKeywords();
+        const refreshWatchedCollection = () => (
+             watchedKeywordsCollection = db.getWatchedKeywords()
+        );
+        if (keyword) {
+           try {
+             db.removeWatchedKeyword(interaction.member.id, server.id).then(resp => {
+                 refreshWatchedCollection();
+                 interaction.reply({ content: `Something went horribly wrong while removing keyword.`, ephemeral: true });
+             });
+           } catch (error) {
+              interaction.reply({ content: `Something went horribly wrong while removing keyword.`, ephemeral: true });
+           }
+         }
+        } else {
+          try {
+            db.removeWatchedKeywords(interaction.member.id, server.id, keyword).then(resp => {
+               refreshWatchedCollection();
+               interaction.reply({ content: `Something went horribly wrong while removing all keywords.`, ephemeral: true });
+            });
+          } catch (error) {
+               interaction.reply({ content: `Something went horribly wrong while removing all keywords.`, ephemeral: true });
+          }
+        }
+    },
+};
